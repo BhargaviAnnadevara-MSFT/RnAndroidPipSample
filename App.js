a162a1deb2425c76b92194f883f248ce47320c3f
Bhargavi Annadevara <bhannade@microsoft.com>
2022-07-29 12:58:19 +0530

    remove details screen

--- App.js
+++ App.js
@@ -18,15 +18,6 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      
-    </View>
-  );
-}
-
 const Stack = createNativeStackNavigator();
 
 function App() {
